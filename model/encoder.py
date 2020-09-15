Added debug output, fixes in loss scaling and encoder decoder architectures

model/encoder.py
@@ -21,7 +21,7 @@ class EncoderDecoder(nn.Module):
         padw = 1
         self.role = role
         if role == "encoder":
-            sequence = [nn.Conv2d(in_channels, ndf, kernel_size=kw, stride=2, padding=padw), nn.ReLU()]
+            sequence = [nn.Conv2d(in_channels, ndf, kernel_size=kw, stride=2, padding=padw), norm_layer(ndf), nn.ReLU()]
             nf_mult = 1
             nf_mult_prev = 1
             for n in range(1, n_layers):  # gradually increase the number of filters
@@ -33,7 +33,7 @@ class EncoderDecoder(nn.Module):
                     nn.ReLU()
                 ]
         else:
-            sequence = [nn.ReLU(), nn.ConvTranspose2d(ndf, in_channels, kernel_size=kw, stride=2, padding=padw)]
+            sequence = [nn.ConvTranspose2d(ndf, in_channels, kernel_size=kw, stride=2, padding=padw)]
             nf_mult = 1
             nf_mult_prev = 1
             for n in range(1, n_layers):  # gradually increase the number of filters

optim/losses.py
@@ -1,9 +1,14 @@
 import torch.nn as nn
 import torch
 
+rec_loss_map = {
+	'L1':'rec',
+	'BCE':'logits'
+}
+
 def kld_loss(mu, logvar):
 	l = -0.5*torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-	return l
+	return l/mu.numel()
 
 def build_loss(loss):
 	if loss=="L1":

test.py
@@ -1,10 +1,11 @@
 import argparse
 from model import AnoVAEGAN
 from data import MNISTDataset
-from utils import combine_detect_image, save_compare
+from utils import save_batch_output
 from torch.utils.data import DataLoader
 import torch
 from tqdm import tqdm
+import random
 
 
 def parse_args():
@@ -20,14 +21,14 @@ def parse_args():
                         type=str)
     parser.add_argument('--batch-size',
                         help="Batch size",
-                        default=32,
+                        default=16,
                         type=int)
     parser.add_argument('--detect-thresh',
                         help='Detection threshold',
-                        default=0.85,
+                        default=0.3,
                         type=float)
     parser.add_argument('--n-plot',
-                        help='Number of images to plot and save',
+                        help='Number of batches to plot and save',
                         default=12,
                         type=int)
     parser.add_argument('--save-size',
@@ -54,19 +55,15 @@ def main():
     dataset = MNISTDataset(
         split='test',
         input_size=config.DATASET.INPUT_SIZE)
-
+    random.shuffle(dataset.data)
+    dataset.data = dataset.data[:args.n_plot*args.batch_size]
     test_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
 
     pbar = tqdm(test_loader)
-    comp = []
-    for img in pbar:
+    for i, img in enumerate(pbar):
         img = img.to(device)
         out = model(img)
-        detection = (torch.abs(out['rec']-img)>args.detect_thresh).int()
-        img, detect_image, rec = combine_detect_image(img, detection, out['rec'])
-        comp += [(i,d, r) for i,d, r in zip(img, detect_image, rec)]
-
-    save_compare(args.save_size, comp[:args.n_plot], args.output_dir)
+        save_batch_output(img, out['rec'], args.detect_thresh, args.save_size, args.output_dir, 'test_batch_{}'.format(i))
 
 if __name__ == '__main__':
     main()

train.py
@@ -3,11 +3,11 @@ from tqdm import tqdm
 import os
 import torch
 from torch.utils.data import DataLoader
-from optim import build_loss, build_opt
+from optim import build_loss, build_opt, rec_loss_map
 from model import AnoVAEGAN, Discriminator
 from config import config, update_config
 from data import MNISTDataset
-from utils import ExpAvgMeter, Plotter
+from utils import ExpAvgMeter, Plotter, save_batch_output
 
 
 def parse_args():
@@ -56,6 +56,7 @@ def main():
     rec_loss = build_loss(config.LOSS.REC_LOSS)
     prior_loss = build_loss(config.LOSS.PRIOR_LOSS)
     adv_loss = build_loss(config.LOSS.ADV_LOSS)
+    rec_key = rec_loss_map[config.LOSS.REC_LOSS]
 
     loss_meter = ExpAvgMeter(0.98)
     rec_loss_meter = ExpAvgMeter(0.98)
@@ -73,9 +74,9 @@ def main():
             valid = torch.full((img.shape[0],1), 1, dtype=torch.float, device=device)
             fake = torch.full((img.shape[0],1), 0, dtype=torch.float, device=device)
             out = model(img)
-            rec_l = rec_loss(out['rec'], img)
+            rec_l = rec_loss(out[rec_key], img)
             prior_l = prior_loss(out['mu'], out['logvar'])
-            adv_l = adv_loss(discriminator(out['logits']), valid)
+            adv_l = adv_loss(discriminator(out['rec']), valid)
             loss =  config.LOSS.REC_LOSS_COEFF * rec_l \
                     + config.LOSS.PRIOR_LOSS_COEFF * prior_l \
                     + config.LOSS.ADV_LOSS_COEFF * adv_l
@@ -85,7 +86,7 @@ def main():
             model_opt.step()
 
             real_loss = adv_loss(discriminator(img), valid)
-            fake_loss = adv_loss(discriminator(out['logits'].detach()), fake)
+            fake_loss = adv_loss(discriminator(out['rec'].detach()), fake)
             disc_loss = 0.5 * (real_loss + fake_loss)
 
             disc_opt.zero_grad()
@@ -103,6 +104,15 @@ def main():
                 plotter.plot("Loss", step, rec_loss_meter.value, "Rec loss", "Step", "Value")
                 plotter.plot("Loss", step, prior_loss_meter.value, "Prior loss", "Step", "Value")
                 plotter.plot("Loss", step, adv_loss_meter.value, "Adv loss", "Step", "Value")
+
+            if config.DEBUG.USE and step%config.DEBUG.DEBUG_EVERY == 0:
+                save_batch_output(
+                    img,
+                    out['rec'],
+                    config.DEBUG.DETECT_THRESH,
+                    config.DEBUG.SAVE_SIZE,
+                    config.OUTPUT_DIR,
+                    'batch_{}'.format(step))
         save_path = os.path.join(config.OUTPUT_DIR, config.EXP_NAME + "_checkpoint.pth")
         torch.save({
             'cfg':config,

utils/postprocess.py
@@ -18,7 +18,7 @@ def combine_detect_image(img, detection, rec):
     out = out + detection
     return img.cpu().numpy(), out.cpu().numpy(), to_RGB(rec.detach()).cpu().numpy()
 
-def save_compare(size, list_comps, output_dir):
+def save_compare(size, list_comps, output_dir, suffix):
     l = len(list_comps)
     grid = np.ones((l*(size+20), 3*size+40 , 3))
     for ind, ims in enumerate(list_comps):
@@ -30,5 +30,11 @@ def save_compare(size, list_comps, output_dir):
         grid[(size+20)*ind:size*(ind+1)+ind*20,2*size+40:, :] = im2
     grid *= 255
     pil_image = Image.fromarray(grid.astype(np.uint8))
-    with open(os.path.join(output_dir, 'test_results.png'), mode='wb') as f:
+    with open(os.path.join(output_dir, suffix + '_output.png'), mode='wb') as f:
         pil_image.save(f, 'PNG')
+
+def save_batch_output(img, rec, thresh, save_size, output_dir, suffix):
+    detection = (torch.abs(rec-img)>thresh).int()
+    img, detect_image, rec = combine_detect_image(img, detection, rec)
+    comp = [(i,d, r) for i,d, r in zip(img, detect_image, rec)]
+    save_compare(save_size, comp, output_dir, suffix)